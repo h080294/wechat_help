f7924efcc7776266c1639969f4899589765c0be4
JohnHao <>
2018-07-12 02:21:55 +0800

    fix string

--- app/src/main/java/com/johnhao/wechatapp/Main.java
+++ app/src/main/java/com/johnhao/wechatapp/Main.java
@@ -4,17 +4,14 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.os.Environment;
-import android.preference.PreferenceManager;
-import android.support.annotation.NonNull;
 import android.util.Log;
 
 import java.io.File;
 import java.io.FileOutputStream;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedBridge;
@@ -25,15 +22,14 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
  * Created by johnhao on 2018/7/5.
  */
 
-public class Main implements IXposedHookLoadPackage{
+public class Main implements IXposedHookLoadPackage {
 
 
     private static final String TAG = "hookwechat";
-    private static final String WEIXIN = "com.tencent.mm";
-    private Boolean isLogcatOpen;
-    private String replace;
-    private String replace_text;
-
+    private boolean isLogcatOpen = false;
+    private boolean Active = false;
+    private String replace = "";
+    private String replace_text = "";
 
     public static void saveFile(String str, String str2) {
         try {
@@ -53,32 +49,34 @@ public class Main implements IXposedHookLoadPackage{
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
 
-
-        final Context context = (Context)XposedHelpers.callMethod(XposedHelpers.callStaticMethod(XposedHelpers.findClass("android.app.ActivityThread", (ClassLoader)null), "currentActivityThread", new Object[0]), "getSystemContext", new Object[0]);
-        final IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction("com.johnhao.wechatapp.SETTING_CHANGED");
-        context.registerReceiver(new BroadcastReceiver() {
-            public void onReceive(final Context context, final Intent intent) {
-                if (intent != null) {
-                    replace = intent.getExtras().getString("replace_target_text", "this.score+=t");
-                    replace_text = intent.getExtras().getString("replace_text", "this.score+=t+=10");
-                    isLogcatOpen = intent.getExtras().getBoolean("is_wechat_log", false);
-                    Log.d(TAG, "onReceive: " + replace + "\n" + replace_text);
-                }
-            }
-        }, intentFilter);
-
         // Xposed模块自检测
-        if (loadPackageParam.packageName.equals("com.johnhao.wechatapp")){
+        if (loadPackageParam.packageName.equals("com.johnhao.wechatapp")) {
             XposedHelpers.findAndHookMethod("com.johnhao.wechatapp.MainActivity", loadPackageParam.classLoader, "isModuleActive", XC_MethodReplacement.returnConstant(true));
             Log.d(TAG, "handleLoadPackage: " + "自检测");
         }
 
-
-
         // hook代码
-        if (loadPackageParam.packageName.equals(WEIXIN)) {
+        if (loadPackageParam.packageName.equals("com.tencent.mm")) {
             Log.d(TAG, "handleLoadPackage: " + "准备开始hook微信");
+
+            XposedBridge.hookAllMethods(XposedHelpers.findClass("com.tencent.mm.plugin.appbrand.appcache.ao", loadPackageParam.classLoader), "a", new XC_MethodHook() {
+                protected void afterHookedMethod(XC_MethodHook.MethodHookParam methodHookParam) throws Throwable {
+
+                    if (methodHookParam.args.length >= 2 && ((String) methodHookParam.args[1]).equals("game.js")) {
+                        Log.d(TAG, "afterHookedMethod: 开始hook微信小程序");
+                        String obj = (String) methodHookParam.getResult();
+                        saveFile(obj, "game.txt");
+                        Log.d(TAG, "开始替换逻辑: " + "\n before fix: " + replace + "\n after fix: " + replace_text);
+                        obj = obj.replace(replace, replace_text);
+                        saveFile(obj, "gamefixed.txt");
+                        methodHookParam.setResult(obj);
+                        Log.d(TAG, "Hooked End");
+                    }
+                }
+
+                protected void beforeHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+                }
+            });
 
             // 是否打开微信Log开关
             // 微信versionCode='1321' versionName='6.6.7'
@@ -99,7 +97,7 @@ public class Main implements IXposedHookLoadPackage{
                             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                                 param.args[5] = true;
                                 super.afterHookedMethod(param);
-                                Log.i(TAG,"keep_setupXLog参数isLogcatOpen: " +param.args[5]);
+                                Log.i(TAG, "keep_setupXLog参数isLogcatOpen: " + param.args[5]);
                             }
                         });
 
@@ -111,11 +109,11 @@ public class Main implements IXposedHookLoadPackage{
                         new XC_MethodHook() {
                             @Override
                             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                                String str  = (String) param.args[0];
-                                String str2  = (String) param.args[1];
-                                Object[] objArr= (Object[]) param.args[2];
+                                String str = (String) param.args[0];
+                                String str2 = (String) param.args[1];
+                                Object[] objArr = (Object[]) param.args[2];
                                 String format = objArr == null ? str2 : String.format(str2, objArr);
-                                Log.e(TAG+"f"+str,format);
+                                Log.e(TAG + "f" + str, format);
                                 super.beforeHookedMethod(param);
                             }
                         });
@@ -127,15 +125,15 @@ public class Main implements IXposedHookLoadPackage{
                         new XC_MethodHook() {
                             @Override
                             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                                String str  = (String) param.args[0];
-                                String str2  = (String) param.args[1];
-                                Object[] objArr= (Object[]) param.args[2];
+                                String str = (String) param.args[0];
+                                String str2 = (String) param.args[1];
+                                Object[] objArr = (Object[]) param.args[2];
                                 String format = objArr == null ? str2 : String.format(str2, objArr);
                                 if (format == null) {
                                     format = "";
                                 }
 
-                                Log.e(TAG+"e"+str,format);
+                                Log.e(TAG + "e" + str, format);
 
                                 super.beforeHookedMethod(param);
                             }
@@ -148,15 +146,15 @@ public class Main implements IXposedHookLoadPackage{
                         new XC_MethodHook() {
                             @Override
                             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                                String str  = (String) param.args[0];
-                                String str2  = (String) param.args[1];
-                                Object[] objArr= (Object[]) param.args[2];
+                                String str = (String) param.args[0];
+                                String str2 = (String) param.args[1];
+                                Object[] objArr = (Object[]) param.args[2];
                                 String format = objArr == null ? str2 : String.format(str2, objArr);
                                 if (format == null) {
                                     format = "";
                                 }
 
-                                Log.e(TAG+"w"+str,format);
+                                Log.e(TAG + "w" + str, format);
 
                                 super.beforeHookedMethod(param);
                             }
@@ -169,15 +167,15 @@ public class Main implements IXposedHookLoadPackage{
                         new XC_MethodHook() {
                             @Override
                             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                                String str  = (String) param.args[0];
-                                String str2  = (String) param.args[1];
-                                Object[] objArr= (Object[]) param.args[2];
+                                String str = (String) param.args[0];
+                                String str2 = (String) param.args[1];
+                                Object[] objArr = (Object[]) param.args[2];
                                 String format = objArr == null ? str2 : String.format(str2, objArr);
                                 if (format == null) {
                                     format = "";
                                 }
 
-                                Log.e(TAG+"v"+str,format);
+                                Log.e(TAG + "v" + str, format);
 
                                 super.beforeHookedMethod(param);
                             }
@@ -190,45 +188,59 @@ public class Main implements IXposedHookLoadPackage{
                         new XC_MethodHook() {
                             @Override
                             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                                String str  = (String) param.args[0];
-                                String str2  = (String) param.args[1];
-                                Object[] objArr= (Object[]) param.args[2];
+                                String str = (String) param.args[0];
+                                String str2 = (String) param.args[1];
+                                Object[] objArr = (Object[]) param.args[2];
                                 String format = objArr == null ? str2 : String.format(str2, objArr);
                                 if (format == null) {
                                     format = "";
                                 }
 
-                                Log.e(TAG+"i"+str,format);
+                                Log.e(TAG + "i" + str, format);
 
                                 super.beforeHookedMethod(param);
                             }
                         });
+            }
+        }
+
+        final Context context = (Context) XposedHelpers.callMethod(XposedHelpers.callStaticMethod(XposedHelpers.findClass("android.app.ActivityThread", (ClassLoader) null), "currentActivityThread", new Object[0]), "getSystemContext", new Object[0]);
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("com.johnhao.wechatapp.SETTING_CHANGED");
+        context.registerReceiver(new BroadcastReceiver() {
+            public void onReceive(final Context context, final Intent intent) {
+                if (intent != null) {
+                    String mReplace = intent.getExtras().getString("replace_target", "");
+                    String mReplace_text = intent.getExtras().getString("replace_text", "");
+                    String mIsLogcatOpen = intent.getExtras().getString("logOpen", "no");
+                    String mIsAvtice = intent.getExtras().getString("isActive", "yes");
+                    Log.d(TAG, "onReceive: " + mReplace + "\n" + mReplace_text + "\n开关状态 Active: " + mIsAvtice + "\n IsLogcatOpen: " + mIsLogcatOpen);
+
+                    setReplace(mReplace);
+                    setReplace_text(mReplace_text);
+                    setActive(mIsAvtice);
+                    setLogOpen(mIsLogcatOpen);
+                }
 
             }
-
-
-            XposedBridge.hookAllMethods(XposedHelpers.findClass("com.tencent.mm.plugin.appbrand.appcache.ao", loadPackageParam.classLoader), "a", new XC_MethodHook() {
-                protected void afterHookedMethod(XC_MethodHook.MethodHookParam methodHookParam) throws Throwable {
-
-                    if (methodHookParam.args.length >= 2 && ((String) methodHookParam.args[1]).equals("game.js")) {
-                        Log.d(TAG, "afterHookedMethod: 开始hook微信小程序");
-                        String obj = (String) methodHookParam.getResult();
-                        saveFile(obj, "game.txt");
-                        Log.d(TAG, "开始替换逻辑: " + "\n" +  "before fix " + replace + "\n" + replace_text +  "before fix ");
-                        obj = obj.replace(replace, replace_text);
-                        saveFile(obj, "gamefixed.txt");
-                        methodHookParam.setResult(obj);
-                        Log.d(TAG, "Hooked End");
-                    }
-                }
-
-                protected void beforeHookedMethod(MethodHookParam methodHookParam) throws Throwable {
-                }
-            });
-
-        }
+        }, intentFilter);
 
     }
 
+    private void setReplace(String str) {
+        replace = str;
+    }
+
+    private void setReplace_text(String str) {
+        replace_text = str;
+    }
+
+    private void setActive(String str) {
+        Active = str.equalsIgnoreCase("yes");
+    }
+
+    private void setLogOpen(String str) {
+        isLogcatOpen = str.equalsIgnoreCase("yes");
+    }
 
 }
--- app/src/main/java/com/johnhao/wechatapp/MainActivity.java
+++ app/src/main/java/com/johnhao/wechatapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -16,80 +17,83 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private SharedPreferences prefs;
-    private EditText editReplaceTarget;
-    private EditText editReplaceText;
+    private EditText editReplaceTarget,editReplaceText;
     private Button save;
-    private CheckBox checkbox;
-    private CheckBox wechatLog;
+    private CheckBox checkbox, wechatLog;
     private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (isModuleActive()){
-            Toast.makeText(this, "模块已启动", Toast.LENGTH_LONG).show();
-        }
-        else {
-            Toast.makeText(this, "模块未启动", Toast.LENGTH_LONG).show();
-        }
-
-        editReplaceTarget = findViewById(R.id.replace_target);
-        editReplaceText = findViewById(R.id.replace_text);
-        save = findViewById(R.id.btn_save);
-        checkbox = findViewById(R.id.ischecked);
-        wechatLog = findViewById(R.id.wechectlog);
-
-
-        final boolean isCheck = prefs.getBoolean("is_checked", true);
-        boolean iswechecklog = prefs.getBoolean("is_wechat_log", false);
-
+        initViews();
+        checkActive();
         prefs = PreferenceManager.getDefaultSharedPreferences(this);
         editor = prefs.edit();
+
+        if (prefs.getInt("new", 0) == 0) {
+            initPref();
+        }
+
+        checkbox.setChecked(prefs.getString("isActive", "yes").equalsIgnoreCase("yes"));
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!checkbox.isChecked()) {
+                    Toast.makeText(MainActivity.this, "激活才能正常使用", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                String replaceTarget = editReplaceTarget.getText().toString();
+                String replaceText = editReplaceText.getText().toString();
+
+                if (TextUtils.isEmpty(replaceTarget) || TextUtils.isEmpty(replaceText)) {
+                    Toast.makeText(MainActivity.this, "输入不能为空", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                editor.putString("replace_target", replaceTarget);
+                editor.putString("replace_text", replaceText);
+                editor.apply();
+
+                Intent intent = new Intent("com.johnhao.wechatapp.SETTING_CHANGED");
+                intent.putExtra("replace_target", replaceTarget);
+                intent.putExtra("replace_text", replaceText);
+                intent.putExtra("isActive", prefs.getString("isActive", "yes"));
+                intent.putExtra("logOpen", prefs.getString("logOpen", "no"));
+                sendBroadcast(intent);
+
+                Toast.makeText(MainActivity.this, "设置成功", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    editor.putString("isActive", "yes");
+
+                } else {
+                    editor.putString("isActive", "no");
+                }
+                editor.apply();
+            }
+        });
 
         wechatLog.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
                 if (isChecked) {
-                    editor.putBoolean("is_wechat_log", true);
+                    editor.putString("logOpen", "yes");
 
                 } else {
-                    editor.putBoolean("is_wechat_log", false);
+                    editor.putString("logOpen", "no");
                 }
                 editor.apply();
-            }
-        });
-
-        if (isCheck) {
-            checkbox.setChecked(true);
-        }
-
-        if (iswechecklog) {
-            wechatLog.setChecked(true);
-        }
-
-        save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                String replace_target_text = editReplaceTarget.getText().toString();
-                String replace_text = editReplaceText.getText().toString();
-
-
-                if (!checkbox.isChecked()) {
-                    replace_target_text = "this.score+=t";
-                    replace_text = "this.score+=t+=10";
-                    editor.putString("replace_target_text", replace_target_text);
-                    editor.putString("replace_text", replace_text);
-                    editor.putBoolean("is_checked", false);
-                } else {
-                    editor.putString("replace_target_text", replace_target_text);
-                    editor.putString("replace_text", replace_text);
-                    editor.putBoolean("is_checked", true);
-                }
-                editor.apply();
-                getKey();
             }
         });
 
@@ -99,12 +103,31 @@ public class MainActivity extends AppCompatActivity {
         return false;
     }
 
-    private void getKey() {
-        Intent intent = new Intent("com.johnhao.wechatapp.SETTING_CHANGED");
-        intent.putExtra("replace_target_text", prefs.getString("replace_target_text", ""));
-        intent.putExtra("replace_text", prefs.getString("replace_text", ""));
-        intent.putExtra("is_wechat_log", prefs.getBoolean("is_wechat_log", false));
-        sendBroadcast(intent);
+    private void initViews() {
+        editReplaceTarget = findViewById(R.id.replace_target);
+        editReplaceText = findViewById(R.id.replace_text);
+        save = findViewById(R.id.btn_save);
+        checkbox = findViewById(R.id.ischecked);
+        wechatLog = findViewById(R.id.wechectlog);
+    }
+
+    private void initPref() {
+        // 首次安装，写入默认数据
+        editor.putInt("new", 1);
+        editor.putString("isActive", "yes");
+        editor.putString("logOpen", "no");
+        editor.putString("replace_target", "this.diamonds-t");
+        editor.putString("replace_text", "this.diamonds-t");
+        checkbox.setChecked(true);
+    }
+
+    private void checkActive() {
+        if (isModuleActive()){
+            Toast.makeText(this, "模块已启动", Toast.LENGTH_LONG).show();
+        }
+        else {
+            Toast.makeText(this, "模块未启动", Toast.LENGTH_LONG).show();
+        }
     }
 
 }
