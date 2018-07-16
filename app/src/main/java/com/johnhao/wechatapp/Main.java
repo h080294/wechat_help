b7570833bcc601c5a7878554dddbf0d5c1c1d93e
JohnHao <>
2018-07-16 14:36:59 +0800

    add config file

--- app/src/main/java/com/johnhao/wechatapp/Main.java
+++ app/src/main/java/com/johnhao/wechatapp/Main.java
@@ -4,12 +4,17 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -26,10 +31,11 @@ public class Main implements IXposedHookLoadPackage {
 
 
     private static final String TAG = "hookwechat";
-    private boolean isLogcatOpen = false;
-    private boolean Active = false;
-    private String replace = "";
-    private String replace_text = "";
+    private String isLogcatOpen;
+    private String active;
+    private String replace;
+    private String replace_text;
+
 
     public static void saveFile(String str, String str2) {
         try {
@@ -52,35 +58,48 @@ public class Main implements IXposedHookLoadPackage {
         // Xposed模块自检测
         if (loadPackageParam.packageName.equals("com.johnhao.wechatapp")) {
             XposedHelpers.findAndHookMethod("com.johnhao.wechatapp.MainActivity", loadPackageParam.classLoader, "isModuleActive", XC_MethodReplacement.returnConstant(true));
-            Log.d(TAG, "handleLoadPackage: " + "自检测");
+            Log.d(TAG, "自检测");
         }
+
 
         // hook代码
         if (loadPackageParam.packageName.equals("com.tencent.mm")) {
-            Log.d(TAG, "handleLoadPackage: " + "准备开始hook微信");
+            Log.d(TAG, "准备开始hook微信");
 
-            XposedBridge.hookAllMethods(XposedHelpers.findClass("com.tencent.mm.plugin.appbrand.appcache.ao", loadPackageParam.classLoader), "a", new XC_MethodHook() {
-                protected void afterHookedMethod(XC_MethodHook.MethodHookParam methodHookParam) throws Throwable {
+            getConfig();
 
-                    if (methodHookParam.args.length >= 2 && ((String) methodHookParam.args[1]).equals("game.js")) {
-                        Log.d(TAG, "afterHookedMethod: 开始hook微信小程序");
-                        String obj = (String) methodHookParam.getResult();
-                        saveFile(obj, "game.txt");
-                        Log.d(TAG, "开始替换逻辑: " + "\n before fix: " + replace + "\n after fix: " + replace_text);
-                        obj = obj.replace(replace, replace_text);
-                        saveFile(obj, "gamefixed.txt");
-                        methodHookParam.setResult(obj);
-                        Log.d(TAG, "Hooked End");
+            boolean is_Actived = active.equalsIgnoreCase("yes");
+            boolean is_LogcatOpen = isLogcatOpen.equalsIgnoreCase("yes");
+
+            if (is_Actived) {
+                Log.d(TAG, "获取信息  Active：yes");
+
+                XposedBridge.hookAllMethods(XposedHelpers.findClass("com.tencent.mm.plugin.appbrand.appcache.ao", loadPackageParam.classLoader), "a", new XC_MethodHook() {
+                    protected void afterHookedMethod(XC_MethodHook.MethodHookParam methodHookParam) throws Throwable {
+
+                        if (methodHookParam.args.length >= 2 && ((String) methodHookParam.args[1]).equals("game.js")) {
+                            Log.d(TAG, "afterHookedMethod: 开始hook微信小程序");
+                            String obj = (String) methodHookParam.getResult();
+                            saveFile(obj, "game.txt");
+                            Log.d(TAG, "开始替换逻辑: " + "\n before fix: " + replace + "\n after fix: " + replace_text);
+                            obj = obj.replace(replace, replace_text);
+                            saveFile(obj, "gamefixed.txt");
+                            methodHookParam.setResult(obj);
+                            Log.d(TAG, "Hooked End");
+                        }
                     }
-                }
 
-                protected void beforeHookedMethod(MethodHookParam methodHookParam) throws Throwable {
-                }
-            });
+                    protected void beforeHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+                    }
+                });
+
+            }
 
             // 是否打开微信Log开关
             // 微信versionCode='1321' versionName='6.6.7'
-            if (isLogcatOpen) {
+            if (is_LogcatOpen) {
+                Log.d(TAG, "获取信息  logOpen： yes");
+
                 //public static void keep_setupXLog(boolean z, String str, String str2, Integer num, Boolean bool, Boolean bool2, String str3)
                 XposedHelpers.findAndHookMethod("com.tencent.mm.xlog.app.XLogSetup", loadPackageParam.classLoader, "keep_setupXLog",
                         boolean.class, String.class, String.class, Integer.class, Boolean.class,
@@ -205,42 +224,41 @@ public class Main implements IXposedHookLoadPackage {
         }
 
         final Context context = (Context) XposedHelpers.callMethod(XposedHelpers.callStaticMethod(XposedHelpers.findClass("android.app.ActivityThread", (ClassLoader) null), "currentActivityThread", new Object[0]), "getSystemContext", new Object[0]);
-        final IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction("com.johnhao.wechatapp.SETTING_CHANGED");
-        context.registerReceiver(new BroadcastReceiver() {
-            public void onReceive(final Context context, final Intent intent) {
-                if (intent != null) {
-                    String mReplace = intent.getExtras().getString("replace_target", "");
-                    String mReplace_text = intent.getExtras().getString("replace_text", "");
-                    String mIsLogcatOpen = intent.getExtras().getString("logOpen", "no");
-                    String mIsAvtice = intent.getExtras().getString("isActive", "yes");
-                    Log.d(TAG, "onReceive: " + mReplace + "\n" + mReplace_text + "\n开关状态 Active: " + mIsAvtice + "\n IsLogcatOpen: " + mIsLogcatOpen);
 
-                    setReplace(mReplace);
-                    setReplace_text(mReplace_text);
-                    setActive(mIsAvtice);
-                    setLogOpen(mIsLogcatOpen);
+    }
+
+    private void getConfig() {
+        String path = Environment.getExternalStorageDirectory().getPath() + "/config.txt";
+
+        final File file = new File(path);
+        if (file.isDirectory()) {
+            Log.d(TAG, "getConfig: File [/sdcard/config.txt] not exist");
+        } else {
+
+            try {
+                InputStream instream = new FileInputStream(file);
+                InputStreamReader inputReader = new InputStreamReader(instream);
+                BufferedReader bufferedReader = new BufferedReader(inputReader);
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    if (line.contains("isActive")) {
+                        active = line.substring(line.indexOf(":") + 1);
+                    } else if (line.contains("logOpen")) {
+                        isLogcatOpen = line.substring(line.indexOf(":") + 1);
+                    } else if (line.contains("replace_target")) {
+                        replace = line.substring(line.indexOf(":") + 1);
+                    } else if (line.contains("replace_text")) {
+                        replace_text = line.substring(line.indexOf(":") + 1);
+                    }
                 }
-
+                instream.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        }, intentFilter);
+        }
 
     }
-
-    private void setReplace(String str) {
-        replace = str;
-    }
-
-    private void setReplace_text(String str) {
-        replace_text = str;
-    }
-
-    private void setActive(String str) {
-        Active = str.equalsIgnoreCase("yes");
-    }
-
-    private void setLogOpen(String str) {
-        isLogcatOpen = str.equalsIgnoreCase("yes");
-    }
-
 }
+
--- app/src/main/java/com/johnhao/wechatapp/MainActivity.java
+++ app/src/main/java/com/johnhao/wechatapp/MainActivity.java
@@ -2,22 +2,28 @@ package com.johnhao.wechatapp;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Environment;
 import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.Checkable;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "hookwechat";
     private SharedPreferences prefs;
-    private EditText editReplaceTarget,editReplaceText;
+    private EditText editReplaceTarget, editReplaceText;
     private Button save;
     private CheckBox checkbox, wechatLog;
     private SharedPreferences.Editor editor;
@@ -65,6 +71,13 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra("logOpen", prefs.getString("logOpen", "no"));
                 sendBroadcast(intent);
 
+                String isActive = "isActive:" + prefs.getString("isActive", "yes") + "\r\n";
+                String logOpen = "logOpen:" + prefs.getString("logOpen", "no") + "\r\n";
+                String replace_target = "replace_target:" + replaceTarget + "\r\n";
+                String replace_text = "replace_text:" + replaceText + "\r\n";
+                String config = isActive + logOpen + replace_target + replace_text;
+//                saveConfig(config);
+                writeFileToSDCard(config.getBytes(), null, "config.txt", false, false);
                 Toast.makeText(MainActivity.this, "设置成功", Toast.LENGTH_SHORT).show();
             }
         });
@@ -99,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private boolean isModuleActive(){
+    private boolean isModuleActive() {
         return false;
     }
 
@@ -122,12 +135,95 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void checkActive() {
-        if (isModuleActive()){
+        if (isModuleActive()) {
             Toast.makeText(this, "模块已启动", Toast.LENGTH_LONG).show();
-        }
-        else {
+        } else {
             Toast.makeText(this, "模块未启动", Toast.LENGTH_LONG).show();
         }
+    }
+
+
+    public static boolean isSdCardExist() {
+        return Environment.getExternalStorageState().equals(
+                Environment.MEDIA_MOUNTED);
+    }
+
+    /* 此方法为android程序写入sd文件文件，用到了android-annotation的支持库@
+    * @param buffer   写入文件的内容
+    * @param folder   保存文件的文件夹名称,如log；可为null，默认保存在sd卡根目录
+    * @param fileName 文件名称，默认app_log.txt
+    * @param append   是否追加写入，true为追加写入，false为重写文件
+    * @param autoLine 针对追加模式，true为增加时换行，false为增加时不换行
+    */
+    public synchronized static void writeFileToSDCard(@Nullable final byte[] buffer, @Nullable final String folder,
+                                                      @Nullable final String fileName, final boolean append, final boolean autoLine) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                boolean sdCardExist = Environment.getExternalStorageState().equals(
+                        android.os.Environment.MEDIA_MOUNTED);
+                String folderPath = "";
+                if (sdCardExist) {
+                    //TextUtils为android自带的帮助类
+                    if (TextUtils.isEmpty(folder)) {
+                        //如果folder为空，则直接保存在sd卡的根目录
+                        folderPath = Environment.getExternalStorageDirectory()
+                                + File.separator;
+                    } else {
+                        folderPath = Environment.getExternalStorageDirectory()
+                                + File.separator + folder + File.separator;
+                    }
+                } else {
+                    return;
+                }
+
+                File fileDir = new File(folderPath);
+                if (!fileDir.exists()) {
+                    if (!fileDir.mkdirs()) {
+                        return;
+                    }
+                }
+                File file;
+
+                //判断文件名是否为空
+                if (TextUtils.isEmpty(fileName)) {
+                    file = new File(folderPath + "app_log.txt");
+                } else {
+                    file = new File(folderPath + fileName);
+                }
+                RandomAccessFile raf = null;
+                FileOutputStream out = null;
+                try {
+                    if (append) {
+                        //如果为追加则在原来的基础上继续写文件
+                        raf = new RandomAccessFile(file, "rw");
+                        raf.seek(file.length());
+                        raf.write(buffer);
+                        if (autoLine) {
+                            raf.write("\n".getBytes());
+                        }
+                    } else {
+                        //重写文件，覆盖掉原来的数据
+                        out = new FileOutputStream(file);
+                        out.write(buffer);
+                        out.flush();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        if (raf != null) {
+                            raf.close();
+                        }
+                        if (out != null) {
+                            out.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
     }
 
 }
